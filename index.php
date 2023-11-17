57fc8130f88f2cf4ac0c659eb3728d9a73682519
em1llyVilela <emillyyvilela@gmail.com>
2023-11-17 11:46:59 -0300

    cor

--- index.php
+++ index.php
@@ -103,7 +103,7 @@
                             <div class="portfolio-hover">
                                 <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
                             </div>
-                            <img class="img-fluid" src="assets/img/portfolio/1.jpg" alt="..." />
+                            <img class="img-fluid" src="img/mA.jpg" alt="..." />
                         </a>
                         <div class="portfolio-caption">
                             <div class="portfolio-caption-heading">Módulo A</div>
@@ -118,7 +118,7 @@
                             <div class="portfolio-hover">
                                 <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
                             </div>
-                            <img class="img-fluid" src="assets/img/portfolio/2.jpg" alt="..." />
+                            <img class="img-fluid" src="img/mB.png" alt="..." />
                         </a>
                         <div class="portfolio-caption">
                             <div class="portfolio-caption-heading">Módulo B</div>
@@ -134,7 +134,7 @@
                             <div class="portfolio-hover">
                                 <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
                             </div>
-                            <img class="img-fluid" src="assets/img/portfolio/3.jpg" alt="..." />
+                            <img class="img-fluid" src="img/mC.jpg" alt="..." />
                         </a>
                         <div class="portfolio-caption">
                             <div class="portfolio-caption-heading">Módulo C</div>
@@ -390,8 +390,7 @@
     <div class="portfolio-modal modal fade" id="portfolioModal1" tabindex="-1" role="dialog" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
-                <div class="close-modal" data-bs-dismiss="modal"><img src="assets/img/close-icon.svg"
-                        alt="Close modal" /></div>
+                <div class="close-modal" data-bs-dismiss="modal"> <img class="img-fluid" src="img/LOGO PRETA.png" alt="..." /></div>
                 <div class="container">
                     <div class="row justify-content-center">
                         <div class="col-lg-8">
@@ -399,7 +398,7 @@
                                 <!-- Project details-->
                                 <h2 class="text-uppercase">Programação de aplicações web;</h2>
                                 <p class="item-intro text-muted">Módulo A</p>
-                                <img class="img-fluid d-block mx-auto" src="assets/img/portfolio/1.jpg" alt="..." />
+                                <img class="img-fluid d-block mx-auto" src="img/mA.jpg" alt="..." />
                                 <p>Espera-se que o estudante implemente soluções web responsivas que atendam demandas
                                     específicas do mercado de trabalho. A avaliação dos resultados será realizada de
                                     acordo com os critérios
@@ -427,8 +426,7 @@
     <div class="portfolio-modal modal fade" id="portfolioModal2" tabindex="-1" role="dialog" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
-                <div class="close-modal" data-bs-dismiss="modal"><img src="assets/img/close-icon.svg"
-                        alt="Close modal" /></div>
+                <div class="close-modal" data-bs-dismiss="modal"><img class="img-fluid" src="img/LOGO PRETA.png" alt="..." /></div>
                 <div class="container">
                     <div class="row justify-content-center">
                         <div class="col-lg-8">
@@ -436,7 +434,7 @@
                                 <!-- Project details-->
                                 <h2 class="text-uppercase">Infraestrutura e Segurança Cibernética</h2>
                                 <p class="item-intro text-muted">Módulo B.</p>
-                                <img class="img-fluid d-block mx-auto" src="assets/img/portfolio/2.jpg" alt="..." />
+                                <img class="img-fluid d-block mx-auto" src="img/mB.png" alt="..." />
                                 <p>No módulo B, espera-se que o estudante adote boas práticas de programação que
                                     objetivem mitigar
                                     o risco de ataques cibernéticos e exploração de vulnerabilidades de segurança. O
@@ -465,8 +463,7 @@
     <div class="portfolio-modal modal fade" id="portfolioModal3" tabindex="-1" role="dialog" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
-                <div class="close-modal" data-bs-dismiss="modal"><img src="assets/img/close-icon.svg"
-                        alt="Close modal" /></div>
+                <div class="close-modal" data-bs-dismiss="modal"><img class="img-fluid" src="img/LOGO PRETA.png" alt="..." /></div>
                 <div class="container">
                     <div class="row justify-content-center">
                         <div class="col-lg-8">
@@ -474,7 +471,7 @@
                                 <!-- Project details-->
                                 <h2 class="text-uppercase">Inovação Tecnológica</h2>
                                 <p class="item-intro text-muted">Módulo C.</p>
-                                <img class="img-fluid d-block mx-auto" src="assets/img/portfolio/3.jpg" alt="..." />
+                                <img class="img-fluid d-block mx-auto" src="img/mC.jpg" alt="..." />
                                 <p>No Módulo C, Inovação Tecnológica, espera-se que os participantes explorem sua
                                     criatividade e
                                     habilidades de resolução de problemas para propor soluções inovadoras e disruptivas.
