8529ffb636d2cd81c952c7c38d44d43cde757504
brunasant0s-dev <brunaceciliavitorio@gmail.com>
2023-11-17 12:04:25 -0300

    img

--- index.php
+++ index.php
@@ -267,19 +267,19 @@
         <div class="container">
             <div class="row align-items-center">
                 <div class="col-md-3 col-sm-6 my-3">
-                    <a href="#!"><img class="img-fluid img-brand d-block mx-auto" src="img/SENAI_São_Paulo_logo.png"
+                    <a href="#!"><img class="img-fluid img-brand d-block mx-auto" src="img/branco.png"
                             alt="..." aria-label="Microsoft Logo" /></a>
                 </div>
                 <div class="col-md-3 col-sm-6 my-3">
-                    <a href="#!"><img class="img-fluid img-brand d-block mx-auto" src="assets/img/logos/google.svg"
+                    <a href="#!"><img class="img-fluid img-brand d-block mx-auto" src="img/SENAI_São_Paulo_logo.png"
                             alt="..." aria-label="Google Logo" /></a>
                 </div>
                 <div class="col-md-3 col-sm-6 my-3">
-                    <a href="#!"><img class="img-fluid img-brand d-block mx-auto" src="assets/img/logos/facebook.svg"
+                    <a href="#!"><img class="img-fluid img-brand d-block mx-auto" src="img/LOGO ORIGINAL.png"
                             alt="..." aria-label="Facebook Logo" /></a>
                 </div>
                 <div class="col-md-3 col-sm-6 my-3">
-                    <a href="#!"><img class="img-fluid img-brand d-block mx-auto" src="img/LOGO ORIGINAL.png"
+                    <a href="#!"><img class="img-fluid img-brand d-block mx-auto" src="img/branco.png"
                             alt="..." aria-label="IBM Logo" /></a>
                 </div>
             </div>
