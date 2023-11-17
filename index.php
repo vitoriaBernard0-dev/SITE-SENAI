4b55a9be121fd1d4ca30fe2027a4652435e581f3
em1llyVilela <emillyyvilela@gmail.com>
2023-11-17 11:12:02 -0300

    link

--- index.php
+++ index.php
@@ -370,18 +370,17 @@
     <footer class="footer py-4">
         <div class="container">
             <div class="row align-items-center">
-                <div class="col-lg-4 text-lg-start">Copyright &copy; Your Website 2023</div>
+                <div class="col-lg-4 text-lg-start"> SENAI Dev Experience 2023</div>
                 <div class="col-lg-4 my-3 my-lg-0">
-                    <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Twitter"><i
+                    <a class="btn btn-dark btn-social mx-2" href="https://twitter.com/i/flow/login?redirect_after_login=%2Fsenai_taubate" aria-label="Twitter"><i
                             class="fab fa-twitter"></i></a>
-                    <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Facebook"><i
+                    <a class="btn btn-dark btn-social mx-2" href="https://www.facebook.com/senaitaubate/?locale=pt_BR" aria-label="Facebook"><i
                             class="fab fa-facebook-f"></i></a>
-                    <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="LinkedIn"><i
+                    <a class="btn btn-dark btn-social mx-2" href="https://br.linkedin.com/company/escolasenaitaubate" aria-label="LinkedIn"><i
                             class="fab fa-linkedin-in"></i></a>
                 </div>
                 <div class="col-lg-4 text-lg-end">
-                    <a class="link-dark text-decoration-none me-3" href="#!">Privacy Policy</a>
-                    <a class="link-dark text-decoration-none" href="#!">Terms of Use</a>
+                    <a class="link-dark text-decoration-none me-3" href="#!">SENAI Taubaté</a>
                 </div>
             </div>
         </div>
